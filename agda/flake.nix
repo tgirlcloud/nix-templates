cde25d158aefe5d83dd9da37df2118f172df9736
gothiquess <goldengoth.mail@gmail.com>
2025-02-18 09:50:02 -0500

    nixfmt: format files with nixfmt-rfc-style

--- agda/flake.nix
+++ agda/flake.nix
@@ -5,24 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  } @ inputs: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
-        system: function nixpkgs.legacyPackages.${system}
-      );
-  in {
-    packages = forAllSystems (pkgs: {
-      example = pkgs.callPackage ./default.nix {};
-      default = self.packages.${pkgs.stdenv.hostPlatform.system}.example;
-    });
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        example = pkgs.callPackage ./default.nix { };
+        default = self.packages.${pkgs.stdenv.hostPlatform.system}.example;
+      });
 
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.callPackage ./shell.nix { };
-    });
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./shell.nix { };
+      });
 
-    overlays.default = final: _: {example = final.callPackage ./default.nix {};};
-  };
+      overlays.default = final: _: { example = final.callPackage ./default.nix { }; };
+    };
 }
